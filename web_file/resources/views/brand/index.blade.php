0215d6329a91b83f3140116c32b61217632ab5d2
vi_atGP <yemviroth@gmail.com>
2020-10-17 05:36:34 +0700

    llast

--- web_file/resources/views/brand/index.blade.php
+++ web_file/resources/views/brand/index.blade.php
@@ -35,6 +35,28 @@
         </div>
 
       @endforeach
+
+
+      @foreach($brands as $brand)
+        <div class="col-sm-12 col-md-6 col-xl-3 pb-3">
+          <div class="col-12 align-self-center">
+            <img class="shadow card-img-top rounded-circle text-center" src="@if($brand->dsPhoto == ''){{asset('images/profile/noImg.png')}}@else{{asset('images/profile/'.$brand->dsPhoto)}}@endif" alt="Card image cap" style="height: 300px;">
+          </div>
+          <div class="col-12 align-self-center text-center">
+            <hr>
+            <b>
+              <h4>{{$brand->dsLocation}}</h4>
+            </b>
+            ​<p> ឈ្មោះ : <strong> {{$brand->dsName}}</strong> </p>
+            <p>អស័យដ្ឋាន : <strong> {{$brand->dsAddress}}</strong> </p>
+            <p>លេខទូរស័ព្ទ : <strong> {{$brand->dsPhone}}</strong> </p>
+            <p>Page : <strong> {{$brand->dsFb}}</strong> </p>
+            <p>IG : <strong> {{$brand->dsIg}}</strong> </p>
+            <hr>
+          </div>
+        </div>
+
+      @endforeach
         
 
       </div>
